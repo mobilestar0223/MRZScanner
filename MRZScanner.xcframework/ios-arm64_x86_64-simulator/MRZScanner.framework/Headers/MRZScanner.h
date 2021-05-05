99d2e0a7d4a5e97c69377df53bfc0acb9557e53a
Alex Chang <alexchang@Alexs-iMac.local>
2021-05-05 16:19:24 +0800

    Added xcframework

---
+++ MRZScanner.xcframework/ios-arm64_x86_64-simulator/MRZScanner.framework/Headers/MRZScanner.h
@@ -0,0 +1,19 @@
+//
+//  MRZScanner.h
+//  MRZScanner
+//
+//  Created by Alex Chang on 2021/5/5.
+//
+
+#import <Foundation/Foundation.h>
+#import <MRZScanner/LibWrapper.h>
+
+//! Project version number for MRZScanner.
+FOUNDATION_EXPORT double MRZScannerVersionNumber;
+
+//! Project version string for MRZScanner.
+FOUNDATION_EXPORT const unsigned char MRZScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MRZScanner/PublicHeader.h>
+
+
